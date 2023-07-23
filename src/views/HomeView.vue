c6130a3ee6d850260a4d4d60ad77b650ba5ee2dc
jugiros <31550758+jugiros@users.noreply.github.com>
2023-07-23 18:27:11 -0500

    feat: Input validations are added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,36 +18,49 @@
       <form>
         <v-text-field
                 v-model="state.name"
+                :error-messages="v$.name.$errors.map(e => e.$message)"
                 :counter="10"
                 label="Name"
                 required
+                @input="v$.name.$touch"
+                @blur="v$.name.$touch"
         ></v-text-field>
 
         <v-text-field
                 v-model="state.email"
+                :error-messages="v$.email.$errors.map(e => e.$message)"
                 label="E-mail"
                 required
+                @input="v$.email.$touch"
+                @blur="v$.email.$touch"
         ></v-text-field>
 
         <v-select
                 v-model="state.select"
                 :items="items"
+                :error-messages="v$.select.$errors.map(e => e.$message)"
                 label="Item"
                 required
+                @change="v$.select.$touch"
+                @blur="v$.select.$touch"
         ></v-select>
 
         <v-checkbox
                 v-model="state.checkbox"
+                :error-messages="v$.checkbox.$errors.map(e => e.$message)"
                 label="Do you agree?"
                 required
+                @change="v$.checkbox.$touch"
+                @blur="v$.checkbox.$touch"
         ></v-checkbox>
 
         <v-btn
                 class="me-4"
+                @click="v$.$validate"
         >
           submit
         </v-btn>
-        <v-btn>
+        <v-btn @click="clear">
           clear
         </v-btn>
       </form>
@@ -55,8 +68,17 @@
   </v-card>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import {reactive} from 'vue'
+import {useVuelidate} from '@vuelidate/core'
+import {email, helpers, required} from '@vuelidate/validators'
+
+const items = [
+  'Item 1',
+  'Item 2',
+  'Item 3',
+  'Item 4',
+]
 
 const initialState = {
   name: '',
@@ -69,12 +91,23 @@ const state = reactive({
   ...initialState,
 })
 
-const items = [
-  'Item 1',
-  'Item 2',
-  'Item 3',
-  'Item 4',
-]
+const rules = {
+  name: {required: helpers.withMessage('Debe agregar al nombre del usuario', required)},
+  email: {required, email},
+  select: {required},
+  items: {required},
+  checkbox: {required},
+}
+
+const v$ = useVuelidate(rules, state, { $lazy: true })
+
+function clear() {
+  v$.value.$reset()
+
+  for (const [key, value] of Object.entries(initialState)) {
+    state[key] = value
+  }
+}
 </script>
 
 <style>
