29dc3d4bfe8178f20f2c537e8e104ad67755fa29
jugiros <31550758+jugiros@users.noreply.github.com>
2023-07-23 17:38:09 -0500

    feat: Navigation menu updating

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,22 +12,70 @@
       <v-card-title>Busca tu vuelo</v-card-title>
     </v-img>
     <v-card-subtitle class="pt-4">
-      Number 10
+      Agrega los criterios de búsqueda
     </v-card-subtitle>
     <v-card-text>
-      <div>Whitehaven Beach</div>
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <form>
+        <v-text-field
+                v-model="state.name"
+                :counter="10"
+                label="Name"
+                required
+        ></v-text-field>
+
+        <v-text-field
+                v-model="state.email"
+                label="E-mail"
+                required
+        ></v-text-field>
+
+        <v-select
+                v-model="state.select"
+                :items="items"
+                label="Item"
+                required
+        ></v-select>
+
+        <v-checkbox
+                v-model="state.checkbox"
+                label="Do you agree?"
+                required
+        ></v-checkbox>
+
+        <v-btn
+                class="me-4"
+        >
+          submit
+        </v-btn>
+        <v-btn>
+          clear
+        </v-btn>
+      </form>
     </v-card-text>
-    <v-card-actions>
-      <v-btn color="orange">
-        Share
-      </v-btn>
-      <v-btn color="orange">
-        Explore
-      </v-btn>
-    </v-card-actions>
   </v-card>
 </template>
+
+<script setup lang="ts">
+import {reactive} from 'vue'
+
+const initialState = {
+  name: '',
+  email: '',
+  select: null,
+  checkbox: null,
+}
+
+const state = reactive({
+  ...initialState,
+})
+
+const items = [
+  'Item 1',
+  'Item 2',
+  'Item 3',
+  'Item 4',
+]
+</script>
 
 <style>
 .v-img__img, .v-img__picture, .v-img__gradient, .v-img__placeholder, .v-img__error {
