184dde54f218c7402357cd696ccc24355d0d7818
jugiros <31550758+jugiros@users.noreply.github.com>
2023-07-23 01:43:04 -0500

    feat: Adding Pinia

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Pinia
+import { createPinia } from 'pinia'
+
+// Vuetify
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// Pinia
+const pinia = createPinia()
+
+createApp(App).use(router).use(vuetify).use(pinia).mount('#app')
