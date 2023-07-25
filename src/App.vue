487204658a104bc5ecec966593ec4c6c6699c795
jugiros <31550758+jugiros@users.noreply.github.com>
2023-07-25 11:08:55 -0500

    feat: Footer is added

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
   <v-container class="mt-10">
     <router-view />
   </v-container>
+  <footer-layout></footer-layout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from "@/components/MainLayout.vue";
+import FooterLayout from "@/components/FooterLayout.vue";
 </script>
